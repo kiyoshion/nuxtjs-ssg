38576429e9324df31a50b76b69f29cead0ad3f5d
kiyoshion <kiyoshion@gmail.com>
2021-05-06 14:33:43 +0900

    Modify default settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,26 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  mode: 'universal',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxtjs-ssg',
+    titleTemplate: '%s - nuxtjs-ssg',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'nuxtjs-ssg' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://github.com/kiyoshion/nuxtjs-ssg' },
+      { hid: 'og:title', property: 'og:title', content: 'nuxtjs-ssg' },
+      { hid: 'og:description', property: 'og:description', content: 'This is Nuxt.js template.' },
+      { hid: 'og:image', property: 'og:image', content: 'https://example.com/images/ogp/common.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
