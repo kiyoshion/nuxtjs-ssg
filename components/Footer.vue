d1434a8109b7462191c72e0369ff2dc37d587505
kiyoshion <kiyoshion@gmail.com>
2021-05-06 14:25:28 +0900

    Add default components

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  footer.l-footer
+    .l-footer__inner
+      .l-footer__logo
+      nav.l-footer__nav
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  header.l-header
+    .l-header__inner
+      .l-header__logo
+      nav.l-header__nav
+</template>
