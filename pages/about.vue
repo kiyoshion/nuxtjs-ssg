24e17c218c65becd365367eaf1b0d78d883730cf
kiyoshion <kiyoshion@gmail.com>
2021-05-06 14:31:53 +0900

    Add sample page (about.vue)

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .p-about
+</template>
+
+<script>
+import Meta from '../assets/js/mixin/meta';
+
+export default {
+  mixins: [Meta],
+  data () {
+    return {
+      meta: {
+        title: 'about',
+        description: 'about page',
+        type: 'article',
+        url: '',
+        image: '',
+      }
+    }
+  }
+}
+</script>
